500409dec40c0a1ede238028c9a5720dfdc75115
PiotrGrzyb <piotrg6711@gmail.com>
2021-10-22 11:38:52 +0200

    Processing hands

--- Mouse.py
+++ Mouse.py
@@ -4,8 +4,17 @@ import mediapipe
 
 def main():
     cap = cv2.VideoCapture(0)
+    mediapipeHands = mediapipe.solutions.hands
+    hands = mediapipeHands.Hands()
+
     while True:
         success, img = cap.read()
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        result = hands.process(imgRGB)
+
+        #If we want to see data that the processors of hands see in terminal
+        #print(result.multi_hand_landmarks)
+
         cv2.imshow("Cam", img)
         cv2.waitKey(1)
 
