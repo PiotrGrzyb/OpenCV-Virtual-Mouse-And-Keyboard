4c3427f74c02d111794d228bfc622e97ade7c327
PiotrGrzyb <piotrg6711@gmail.com>
2021-11-01 15:09:09 +0100

    Updated info and drawing circles

--- Mouse.py
+++ Mouse.py
@@ -1,20 +1,43 @@
 import cv2
 import mediapipe
+import time
 
 
 def main():
     cap = cv2.VideoCapture(0)
     mediapipeHands = mediapipe.solutions.hands
     hands = mediapipeHands.Hands()
+    mpDraw = mediapipe.solutions.drawing_utils
+    mpDrawStyle = mediapipe.solutions.drawing_styles
+
+    previousTime = 0
+    currentTime = 0
 
     while True:
         success, img = cap.read()
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         result = hands.process(imgRGB)
 
-        #If we want to see data that the processors of hands see in terminal
-        #print(result.multi_hand_landmarks)
+        # If we want to see data that the processors of hands see in terminal
+        # print(result.multi_hand_landmarks)
+        if result.multi_hand_landmarks:
+            for handLandmarks in result.multi_hand_landmarks:
+                for id, lm in enumerate(handLandmarks.landmark):
+                    #print(id, lm)
+                    h , w, c = img.shape
+                    cx, cy = int(lm.x*w), int(lm.y*h)
+                    print("[ID:", id , "]","X:", cx,"[Y]", cy)
 
+        if result.multi_hand_landmarks:
+            for handLandmarks in result.multi_hand_landmarks:
+                mpDraw.draw_landmarks(img, handLandmarks)
+
+        currentTime = time.time()
+        fps = 1 / (currentTime - previousTime)
+        previousTime = currentTime
+
+        cv2.putText(img, str(int(fps)), (10, 70),
+                    cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
         cv2.imshow("Cam", img)
         cv2.waitKey(1)
 
